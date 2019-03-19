02b4bf1e5692678a290d9f1748ffeb4ff641aed5
unknown <m.y.s.tenhage@student.utwente.nl>
2019-03-19 21:21:47 +0100

    maps in layout added

--- app/src/main/java/nl/saproto/anke/HelperProvider.java
+++ app/src/main/java/nl/saproto/anke/HelperProvider.java
@@ -2,6 +2,9 @@ package nl.saproto.anke;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.widget.Toast;
 
 import net.openid.appauth.AuthState;
@@ -9,8 +12,10 @@ import net.openid.appauth.AuthState;
 import org.json.JSONException;
 
 public class HelperProvider {
+    private CoordinatorLayout coordinatorLayout;
 
     public static void showToast(Context context, String message) {
+
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(context, message, duration);
         toast.show();
